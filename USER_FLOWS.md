1a6c2780130aa52352ee2ce84593cf77bf2f83f0
humzahkiani <89326566+humzahkiani@users.noreply.github.com>
2026-05-14 16:48:08 -0700

    feat: allow user to save new commmands with phrase

--- USER_FLOWS.md
+++ USER_FLOWS.md
@@ -1,8 +1,10 @@
 # User Flows
 
 ## Core
-1. As a dev working in the terminal, when I forget a specific command I want to use I want to be able to enter a phrase that describes what the command does and have the commands that match the phrase returned and ranked by how well they match. 
+1. As a dev working, when I forget a specific terminal command I want to use I want to be able to enter a phrase that describes what the command does and have the commands that match the phrase returned and ranked by how well they match
+2. As a dev working in the terminal, I want to be able to save new commands so that I can search for them when I forget them
 
 ## Future
 
-1. As a dev working in the terminal, when uhh does not match a phrase to a command, I want to be able to add it as a phrase for an existing command or create a new command. 
+1. As a dev working in the terminal, when uhh does not match my phrase to the correct command but the command has been saved, I want to be able to add my phrase to the saved command so future match's will retrieve it
+2. As a dev, I want to be able to designate placeholder sections in the commands that I save for the arguments of the command
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"cmp"
 	"errors"
+	"flag"
 	"fmt"
 	"io/fs"
 	"os"
@@ -44,43 +45,57 @@ func main() {
 }
 
 func run() error {
-	configDir := os.Getenv("XDG_CONFIG_HOME")
-	if configDir == "" {
-		home, err := os.UserHomeDir()
-		if err != nil {
-			return fmt.Errorf("locate home dir: %w", err)
-		}
-		configDir = filepath.Join(home, ".config")
-	}
-	path := filepath.Join(configDir, "uhh", "commands.yaml")
-
-	data, err := os.ReadFile(path)
-
-	if errors.Is(err, fs.ErrNotExist) {
-		return fmt.Errorf("no command file found; create one at %s", path)
-	}
-	if err != nil {
-		return fmt.Errorf("read commands file: %w", err)
-	}
-
-	var commands []Command
-	err = yaml.Unmarshal(data, &commands)
-	if err != nil {
-		return fmt.Errorf("parse saved commands: %w", err)
-	}
-	if len(commands) == 0 {
-		return fmt.Errorf("no saved commands found; add one to %s", path)
-	}
-
 	args := os.Args[1:]
+	if len(args) == 0 {
+		return fmt.Errorf("no args provided")
+	}
 
-	if len(args) != 1 {
-		return errors.New("must supply exactly one search phrase in quotes")
+	// cli subcommand/arg handling
+	switch args[0] {
+	case "save":
+		fs := flag.NewFlagSet("save", flag.ContinueOnError)
+		cmd := fs.String("cmd", "", "command shell code")
+		phrases := fs.String("phrases", "", "comma-delimited phrases that describe what the command does")
+
+		err := fs.Parse(args[1:])
+		if err != nil {
+			return err
+		}
+
+		if len(fs.Args()) > 0 {
+			return fmt.Errorf("unexpected positional argument(s): %q", fs.Args())
+		}
+		if *cmd == "" || *phrases == "" {
+			return fmt.Errorf("all required args not provided: --cmd, --phrases ")
+		}
+
+		err = save(*cmd, strings.Split(*phrases, ","))
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		fs := flag.NewFlagSet("find", flag.ContinueOnError)
+		_ = fs.Parse(args[0:])
+		if len(fs.Args()) != 1 {
+			fmt.Println(fs.Args())
+			return errors.New("unexpected no. of positional args; provide exactly one search phrase argument in quotations")
+		}
+	}
+
+	// find command (implicit default)
+
+	savedCommands, err := readSavedCommands()
+	if err != nil {
+		return err
+	}
+
+	if len(savedCommands) == 0 {
+		return errors.New("no saved commands found")
 	}
 
 	searchPhrase := args[0]
-
-	matchedAndRankedCommands := matchAndRank(searchPhrase, commands)
+	matchedAndRankedCommands := matchAndRank(searchPhrase, savedCommands)
 
 	if len(matchedAndRankedCommands.CommandMatches) == 0 {
 		return errNoMatches
@@ -96,6 +111,83 @@ func run() error {
 	}
 
 	return outputTableWriter.Flush()
+}
+
+func save(command string, phrases []string) error {
+	if strings.TrimSpace(command) == "" {
+		return errors.New("save: input command is empty/whitespace only")
+	}
+
+	for _, p := range phrases {
+		if strings.TrimSpace(p) == "" {
+			return errors.New("save: one or more phrases is empty/whitespace only")
+		}
+	}
+
+	savedCommands, err := readSavedCommands()
+	if err != nil {
+		return fmt.Errorf("save: %w", err)
+	}
+	for _, c := range savedCommands {
+		if strings.EqualFold(strings.TrimSpace(c.Cmd), strings.TrimSpace(command)) {
+			return errors.New("save: command already exists")
+		}
+	}
+
+	newCommand := Command{Cmd: command, Phrases: phrases}
+	updatedCommands := append(savedCommands, newCommand)
+
+	data, err := yaml.Marshal(updatedCommands)
+	if err != nil {
+		return fmt.Errorf("save: marshal commands: %w", err)
+	}
+
+	path, err := getCommandsFilePath()
+	if err != nil {
+		return fmt.Errorf("save: %w", err)
+	}
+
+	if err := os.WriteFile(path, data, 0o644); err != nil {
+		return fmt.Errorf("save: write commands file: %w", err)
+	}
+
+	fmt.Printf("successfully saved command: '%v'\n", newCommand.Cmd)
+	return nil
+}
+
+func readSavedCommands() ([]Command, error) {
+	path, err := getCommandsFilePath()
+	if err != nil {
+		return nil, fmt.Errorf("read saved commands: %w", err)
+	}
+
+	data, err := os.ReadFile(path)
+	if errors.Is(err, fs.ErrNotExist) {
+		return nil, fmt.Errorf("read saved commands: %w", err)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("read saved commands: %w", err)
+	}
+	var commands []Command
+	err = yaml.Unmarshal(data, &commands)
+	if err != nil {
+		return nil, fmt.Errorf("read saved commands: %w", err)
+	}
+	return commands, nil
+}
+
+func getCommandsFilePath() (string, error) {
+	configDir := os.Getenv("XDG_CONFIG_HOME")
+	if configDir == "" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", fmt.Errorf("locate home dir: %w", err)
+		}
+		configDir = filepath.Join(home, ".config")
+	}
+	path := filepath.Join(configDir, "uhh", "commands.yaml")
+
+	return path, nil
 }
 
 func matchAndRank(phrase string, commands []Command) CommandMatches {
